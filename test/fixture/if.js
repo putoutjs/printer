2661d5958e93efd2c7427adf0aa05a4fb2a040e9
coderaiser <coderaiser@cloudcmd.io>
2023-03-13 12:57:15 +0200

    feature: engine-printer: add support of IfStatement

---
+++ test/fixture/if.js
@@ -0,0 +1,4 @@
+if (a) {
+    const b = fn1();
+    fn2(a);
+}
--- test/printer.js
+++ test/printer.js
@@ -4,17 +4,30 @@ const montag = require('montag');
 
 const tryCatch = require('try-catch');
 
-const {test} = require('supertape');
+const {extend} = require('supertape');
 const {parse} = require('putout');
 
 const {readFixtures} = require('./fixture');
 const {print} = require('..');
+
+const test = extend({
+    print: ({fail, equal}) => (fixture) => {
+        const ast = parse(fixture);
+        const [error, source] = tryCatch(print, ast);
+        
+        if (error)
+            return fail(error.message);
+        
+        return equal(source, fixture);
+    },
+});
 
 const fixture = readFixtures([
     'arrow',
     'arrow-fix',
     'arrow-block-return',
     'arrow-block-return-fix',
+    'if',
     'string',
     'unknown',
 ]);
@@ -40,6 +53,11 @@ test('putout: printer: string', (t) => {
     const source = print(ast);
     
     t.equal(source, fixture.string);
+    t.end();
+});
+
+test('putout: printer: if', (t) => {
+    t.print(fixture.if);
     t.end();
 });
 
