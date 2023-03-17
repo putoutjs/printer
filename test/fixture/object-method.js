71360b40992004ced065f963efe15d5b596e5652
coderaiser <coderaiser@cloudcmd.io>
2023-03-17 13:02:41 +0200

    feature: @putout/printer: improve support of ObjectExpression

--- test/fixture/object-method.js
+++ test/fixture/object-method.js
@@ -3,3 +3,10 @@
 module.exports.traverse = ({push}) => ({
     VariableDeclarator(path, hello) {},
 });
+
+parse({
+    exit(path) {
+        const {node} = path;
+        convertNodeComments(node);
+    },
+});
