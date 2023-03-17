09e1a5c8eb5f9455af96646b51f32b9e8cc55055
coderaiser <coderaiser@cloudcmd.io>
2023-03-17 16:37:23 +0200

    feature: @putout/printer: improve support of ObjectExpression

--- test/fixture/object-expression.js
+++ test/fixture/object-expression.js
@@ -5,3 +5,16 @@ module.exports.report = () => `Unexpected 'debugger' statement`;
 module.exports.replace = () => ({
     debugger: '',
 });
+
+__putout_processor_json({
+    'jobs': {
+        'build': {
+            'runs-on': 'ubuntu-latest',
+            'strategy': {
+                'matrix': {
+                    'node-version': ['16.x', '18.x', '19.x'],
+                },
+            },
+        },
+    },
+});
