b4076e9bd3c6119aace2ac1097ed625cccd67027
coderaiser <coderaiser@cloudcmd.io>
2023-03-31 17:48:53 +0300

    feature: @putout/printer: add support of TSAnyKeyword

---
+++ lib/tokenize/typescript/fixture/any.ts
@@ -0,0 +1,7 @@
+this.mappings.forEach(function (this: any, mapping: any, i: any) {
+    var sliced = mapping.slice(this, start, i);
+    
+    if (sliced) {
+        newMappings.push(sliced);
+    }
+}, this);
