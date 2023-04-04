c8fc7b75d49045652eb0c3d2531b050bdd396eed
coderaiser <coderaiser@cloudcmd.io>
2023-04-04 17:22:19 +0300

    feature: @putout/printer: improve support of tuples

--- rules/add-args/index.js
+++ rules/add-args/index.js
@@ -3,28 +3,20 @@
 const {operator} = require('putout');
 const {addArgs} = operator;
 
-const parents = ['__ = __', 'module.exports.__a = (path, __object) => __body', 'module.exports.__a = (path) => __body', 'function __(path) {}', 'function __(path, __object) {}'];
+const parents = [
+    '__ = __',
+    'module.exports.__a = (path, __object) => __body',
+    'module.exports.__a = (path) => __body',
+    'function __(path) {}',
+    'function __(path, __object) {}',
+];
 
 module.exports = addArgs({
     path: ['path', 'module.exports.__a = () => __body'],
-    maybe: [
-        '{maybe}',
-        parents,
-    ],
-    write: [
-        '{write}',
-        parents,
-    ],
-    print: [
-        '{print}',
-        parents,
-    ],
-    indent: [
-        '{indent}',
-        parents,
-    ],
-    compute: [
-        '{compute}',
-        parents,
-    ],
+    maybe: ['{maybe}', parents],
+    write: ['{write}', parents],
+    print: ['{print}', parents],
+    indent: ['{indent}', parents],
+    compute: ['{compute}', parents],
 });
+
--- test/fixture/array-expression.js
+++ test/fixture/array-expression.js
@@ -13,3 +13,14 @@ run(['a', 'b', 'c'], '--fix');
 const x = {
     'prepublishOnly': () => run(['lint', 'test']),
 };
+
+const parents = [
+    '__ = __',
+    'module.exports.__a = (path, __object) => __body',
+    'module.exports.__a = (path, __object) => __body',
+];
+
+module.exports = addArgs({
+    path: ['path', 'module.exports.__a = () => __body'],
+    maybe: ['{maybe}', parens],
+});
