db62133b1c4dc877bd6867262c81a6f1dba50487
coderaiser <coderaiser@cloudcmd.io>
2023-04-06 13:18:17 +0300

    feature: @putout/printer: add support of chaining

--- test/fixture/chain.js
+++ test/fixture/chain.js
@@ -1,2 +1,20 @@
-hello.get('hello world')
+hello
+    .get('hello world')
     .set('how come');
+
+const hasDoubleQuote = (a) => createCheckRegExp('"').test(a);
+this.mappings.forEach(function (mapping, i) {});
+
+var sliced = mapping.slice(this, start, i);
+console.log(hello);
+
+module.exports.report = ({idName}) => `Useless variable declaration with name "${idName}"`;
+
+const assignIterable = (__i) => `const __a = __b[${__i.name}]`;
+delete hello.world;
+
+this.print();
+
+const {stopAll} = createMockImport(import.meta.url);
+
+this._maxLength = a;
