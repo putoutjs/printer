b14f85d05f4802b6fa63bf35ccb5b972bbc63664
coderaiser <coderaiser@cloudcmd.io>
2023-04-12 17:57:26 +0300

    feature: @putout/printer: add support of TSQualifiedName

---
+++ lib/tokenize/typescript/fixture/ts-qualified-name.ts
@@ -0,0 +1,6 @@
+type Pos = namedTypes.Position;
+type Loc = namedTypes.SourceLocation;
+
+export default class Mapping {
+    constructor (public sourceLines: Lines, public sourceLoc: Loc, public targetLoc: Loc) {}
+};
