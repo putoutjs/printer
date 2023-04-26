9ac6132d3d4901ae9185691eb18fda8d6da62545
coderaiser <coderaiser@cloudcmd.io>
2023-04-26 11:32:01 +0300

    feature: @putout/printer: improve support of SwitchCase

--- test/fixture/switch-statement.js
+++ test/fixture/switch-statement.js
@@ -10,3 +10,17 @@ case 'hello': {
     break;
 }
 }
+
+switch (value.type) {
+case 'Identifier':
+    declare(path, value.name);
+    break;
+    
+case 'AssignmentPattern':
+    if (path.node.shorthand)
+        declare(path, key.name);
+    else
+        declare(path, valuePath.node.left.name);
+    
+    break;
+}
