c2ff505d9ae176df7e93b4fd694a33f05dd00700
coderaiser <coderaiser@cloudcmd.io>
2023-05-02 13:07:27 +0300

    feature: @putout/printer: ImportsDeclaration: improve

--- test/fixture/import-declaration.js
+++ test/fixture/import-declaration.js
@@ -1,10 +1,5 @@
-import fs, {
-    readFile,
-} from 'fs/promises';
+import fs, {readFile} from 'fs/promises';
 import 'supertape/bin/supertape';
-import {
-    compile,
-    parse,
-} from './index.js';
+import {compile, parse} from './index.js';
 
 export function hello() {}
