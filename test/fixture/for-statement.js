90604895915c509c09540a9cefe07f8a9d148f89
coderaiser <coderaiser@cloudcmd.io>
2023-05-03 14:25:50 +0300

    feature: @putout/printer: improve support of ForStatement

--- test/fixture/for-statement.js
+++ test/fixture/for-statement.js
@@ -2,3 +2,15 @@ for (let i = 0; i < n; i++) {}
 
 for (;;)
     break;
+
+for (;;)
+    if (a) {
+        __c4['🧨'](3, 8);
+        continue;
+    }
+
+for (;;) {
+    const a = 5;
+    __c4['🧨'](7, 4);
+    continue;
+}
