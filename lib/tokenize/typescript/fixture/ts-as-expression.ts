b0aa0ffa99002a0cf16b1a3670b404faade06d5e
coderaiser <coderaiser@cloudcmd.io>
2023-05-11 14:50:32 +0300

    feature: @putout/printer: TSAsExpression inside MemberExpression: improve support

--- lib/tokenize/typescript/fixture/ts-as-expression.ts
+++ lib/tokenize/typescript/fixture/ts-as-expression.ts
@@ -1,2 +1,6 @@
 const boundaryElement = <HTMLElement>e.target;
 const boundaryElement2 = e.target as HTMLElement;
+
+const status = await (navigator as unknown as IFontAccessNavigator).permissions.request?.({
+    name: 'local-fonts',
+});
