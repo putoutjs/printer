1a9915d5f1a0ace1d2f8d4abc2b577e53323fece
coderaiser <coderaiser@cloudcmd.io>
2023-06-22 15:37:06 +0300

    feature: @putout/printer: TSMethodSignature: add

--- lib/tokenize/typescript/fixture/ts-constructor-signature-declaration.ts
+++ lib/tokenize/typescript/fixture/ts-constructor-signature-declaration.ts
@@ -1,3 +1,6 @@
+/** Params constructor type. */
 export interface IParamsConstructor {
     new(maxLength: number, maxSubParamsLength: number): IParams;
+    fromArray(values: ParamsArray): IParams;
+    fromArray(): IParams;
 }
