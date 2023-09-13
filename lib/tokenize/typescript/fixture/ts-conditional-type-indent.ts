3ac72f4a58927cab92df45cf8c545a4a48553bd2
coderaiser <coderaiser@cloudcmd.io>
2023-09-13 10:26:18 +0300

    feature: @putout/printer: typescript: newline -> breakline

---
+++ lib/tokenize/typescript/fixture/ts-conditional-type-indent.ts
@@ -0,0 +1,10 @@
+export type WithSelectors<S> =
+    S extends {
+        getState: () => infer T;
+    }
+        ? S & {
+            use: {
+                [K in keyof T]: () => T[K];
+            };
+        }
+        : never;
