05d1c9d7843593a633723f7b7e968996dfc79b54
coderaiser <coderaiser@users.noreply.github.com>
2023-10-19 13:49:43 +0000

    chore: @putout/print: actions: lint ☘️

--- test/printer.errors.ts
+++ test/printer.errors.ts
@@ -1,10 +1,10 @@
 import {print} from '..';
 import {types} from '@putout/babel';
+
 const {identifier} = types;
 
 // THROWS Argument of type 'number' is not assignable to parameter of type 'Node'
 print(1);
-
 // THROWS Type '1' has no properties in common with type 'Options'.
 print(identifier('hello'), 1);
 
