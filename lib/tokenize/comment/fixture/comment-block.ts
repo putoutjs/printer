a70d48bb3ff6a7b119b36ca7702b2dc8ced6fadc
coderaiser <coderaiser@cloudcmd.io>
2023-10-24 16:44:53 +0300

    feature: @putout/printer: CommentBlock: improve support

---
+++ lib/tokenize/comment/fixture/comment-block.ts
@@ -0,0 +1,13 @@
+declare module 'xterm' {
+    /**
+     * A string or number representing text font weight.
+     */
+    export type FontWeight = 'normal' | number;
+
+    /**
+     * A string representing log level.
+     */
+    export type LogLevel = 'trace' | 'off';
+}
+
+// hello
