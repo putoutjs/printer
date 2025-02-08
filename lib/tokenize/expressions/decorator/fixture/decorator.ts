bd1a8cd35c9fda13c881c6a89697dd597b959895
coderiaser <coderaiser@cloudcmd.io>
2025-02-08 22:53:45 +0200

    feature: @putout/printer: decorators: comments: improve

--- lib/tokenize/expressions/decorator/fixture/decorator.ts
+++ lib/tokenize/expressions/decorator/fixture/decorator.ts
@@ -1,8 +1,17 @@
+class Disposable {}
+type ITerminal = string;
+type IInstantiationService = string;
+type ICoreBrowserService = string;
+type IRenderService = string;
+
 export class AccessibilityManager extends Disposable {
     constructor(
         private readonly_terminal: ITerminal,
+        // @ts-expect-error
         @IInstantiationService instantiationService: IInstantiationService,
+        // @ts-expect-error
         @ICoreBrowserService private readonly _coreBrowserService: ICoreBrowserService,
+        // @ts-expect-error
         @IRenderService private readonly _renderService: IRenderService
     ) {
         super();
