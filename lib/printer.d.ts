b6adcd1890da12706cd3ed1d3a878d8d10d29db2
coderiaser <coderaiser@cloudcmd.io>
2025-07-26 15:23:46 +0300

    feature: @putout/printer: types: namespace -> declare

--- lib/printer.d.ts
+++ lib/printer.d.ts
@@ -22,16 +22,13 @@ type Print = (input: string | types.Node) => void;
 type Indent = () => void;
 type Traverse = (input: types.Node) => void;
 
+type MaybeCondition = (condition: boolean) => void;
+
+declare const MaybeIndent: {
+    inc: MaybeCondition;
+    dec: MaybeCondition;
+};
 declare function MaybeIndent(condition: boolean): void;
-declare namespace MaybeIndent {
-    type inc = (condition: boolean) => void;
-    type dec = (condition: boolean) => void;
-    
-    export {
-        inc,
-        dec,
-    };
-}
 
 type MaybePrint = (condition: boolean, input: string | types.Node) => void;
 type MaybeTraverse = (condition: boolean, input: types.Node) => void;
