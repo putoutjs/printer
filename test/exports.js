fad8a704ace322a2476fecab2fd6ab08d5a35cdf
coderiaser <coderaiser@cloudcmd.io>
2026-01-20 22:04:22 +0200

    chore: lint

--- test/exports.js
+++ test/exports.js
@@ -2,7 +2,7 @@ import {test} from 'supertape';
 
 test('printer: exports: is', async (t) => {
     const exported = await import('@putout/printer/is');
-    const is = await import('../lib/tokenize/is.js');
+    const is = await import('#is');
     
     t.equal(exported, is);
     t.end();
