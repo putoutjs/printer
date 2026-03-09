fdd3a49403280fbb940c4909ee522027aaca01c0
coderiaser <coderaiser@cloudcmd.io>
2026-03-09 16:41:47 +0200

    test: @putout/printer: type-checker: report: coverage

--- test/create-test.js
+++ test/create-test.js
@@ -1,10 +1,12 @@
 import {extend} from 'supertape';
+import * as strip from '@supertape/operator-strip';
 import {printExtension} from '#test/printer';
 import {readFixtures} from '#test/fixture';
 
 export const createTest = (dir) => {
     const fixture = readFixtures(dir);
     const test = extend({
+        ...strip,
         print: printExtension,
     });
     
