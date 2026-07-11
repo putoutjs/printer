4cc792a02834db05cd78973b8868838f0f2980f7
coderiaser <coderaiser@cloudcmd.io>
2026-07-12 01:15:58 +0300

    feature: @putout/printer: TSParameterProperty: Decorator

--- lib/tokenize/expressions/class/fixture/class-decorator.ts
+++ lib/tokenize/expressions/class/fixture/class-decorator.ts
@@ -1,2 +1,9 @@
 @Controller('api/v1/parse')
 export class ParseController {}
+
+@Injectable()
+export class GithubService {
+    constructor(
+        @Inject('OCTOKIT') private readonly octokit: Octokit
+    ) {}
+}
